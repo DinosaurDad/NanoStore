25b68e7e88c296fa5433dee82c64d3be03e2cc1a
DinosaurDad <523433+DinosaurDad@users.noreply.github.com>
2021-11-23 15:29:02 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,15 +1,14 @@
 // swift-tools-version:5.5
 import PackageDescription
-import Foundation
 
 let package = Package(
     name: "NanoStore",
+    platforms: [
+      .iOS(.v12)
+    ],
     products: [
         .library(
           name: "NanoStore",
-            platforms: [
-              .iOS(.v12)
-            ],
           targets: ["NanoStore"])
     ],
     targets: [
